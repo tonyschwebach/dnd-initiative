5d9ba329d10f00b9868a71ba1158898ac9095897
T Schway <tony.schwebach@gmail.com>
2021-03-26 21:48:17 -0400

    styled characters hero font

--- client/src/components/CharactersHeader/CharactersHeader.jsx
+++ client/src/components/CharactersHeader/CharactersHeader.jsx
@@ -5,14 +5,14 @@ const CharactersHeader = () => {
   return (
     <header id="characters-hero" className="text-center rounded-lg">
       <div className="py-20">
-        <h1 class="text-9xl text-gray-50 sm:text-3xl sm:truncate font-mairon">
+        <h1 class="text-7xl text-gray-50 sm:truncate hero-title">
           Characters
         </h1>
         
 
         <button
           type="button"
-          class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          class=" inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           {/* <!-- Heroicon name: solid/pencil --> */}
           <svg
