47ec4fc375ff85499a498f875438738eb5b295d3
T Schway <tony.schwebach@gmail.com>
2021-03-28 16:27:43 -0400

    put character from in character modal

---
+++ client/src/components/CharacterForm/CharacterForm.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+const CharacterForm = () => {
+  return (
+    // <div>
+    //   <div className="md:grid md:grid-cols-3 md:gap-6">
+    //     <div className="mt-5 md:mt-0 md:col-span-2">
+    <form action="#" method="POST" className="w-full mx-0 px-0">
+      <div className="shadow sm:rounded-md sm:overflow-hidden">
+        <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+          <div className="grid grid-cols-3 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                for="first_name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Character Name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                autocomplete="given-name"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+          </div>
+
+          {/* TODO: cloudinary to upload picspics  */}
+          {/* <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Photo
+                  </label>
+
+                  <div className="mt-1 flex items-center">
+                    <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                      <svg
+                        className="h-full w-full text-gray-300"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                      </svg>
+                    </span>
+                    <button
+                      type="button"
+                      className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div> */}
+        </div>
+
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 border-t border-gray-200">
+          <button
+            type="submit"
+            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-600 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default CharacterForm;
