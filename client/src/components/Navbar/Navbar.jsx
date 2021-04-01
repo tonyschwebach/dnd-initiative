8bb983eceb52d6584534dec439dd52f17d322be0
T Schway <tony.schwebach@gmail.com>
2021-04-01 09:12:25 -0400

    navbar logo

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { NavLink, Link } from "react-router-dom";
+import logo from "../../assets/img/d20-icon-15.png";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -69,34 +70,24 @@ const Navbar = () => {
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                alt="Workflow"
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                alt="Workflow"
-              />
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
+              <NavLink
+                to="/"
+                exact
+                // activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex-shrink-0 flex items-center"
+                aria-current="page"
+              >
+                <img className="block  h-8 w-auto flex-1" src={logo} alt="D20 dice" />
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                <NavLink
-                  to="/"
-                  exact
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-
+                <span className="pl-2">DnD Initiative</span>
+              </NavLink>
+            </div>
+            <div className="hidden sm:block sm:ml-6 my-auto">
+              <div className=" space-x-4 ">
                 <NavLink
                   to="/characters"
                   exact
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium  "
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   aria-current="page"
                 >
@@ -115,8 +106,8 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {/* <!-- Profile dropdown --> */}
+          {/* <!-- Profile dropdown --> */}
+          {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="ml-3 relative">
               <div>
                 <button
@@ -146,7 +137,8 @@ const Navbar = () => {
                 leaveTo="transform opacity-0 scale-95"
               >
                 {(ref) => (
-                  <div ref={ref}
+                  <div
+                    ref={ref}
                     className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu"
                     aria-orientation="vertical"
@@ -177,7 +169,7 @@ const Navbar = () => {
                 )}
               </Transition>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
 
